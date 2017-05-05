f9fe36aa29d6e458c2dc5d8d5e56f99ad2d9d1ad
hxd1230 <675556820@qq.com>
2017-05-05 17:59:29 +0800

    Init Project

---
+++ api/SimpleAuthorizationServerProvider.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace api
+{
+    public class SimpleAuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            //string clientId, clientSecret;
+            //context.TryGetBasicCredentials(out clientId, out clientSecret);
+            //if (clientId == "Mobile" && clientSecret == "XiaoMi")
+            //{
+            context.Validated();
+            //}
+            //else
+            //{
+            //    context.Response.StatusCode = 200;
+            //    context.SetError("invalid_client", "无效客户端");
+            //}
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+
+            if (context.UserName.Trim() == "admin" && context.Password.Trim() == "123456")
+            {
+                var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+                identity.AddClaim(new Claim(ClaimTypes.Name, context.UserName));
+                context.Validated(identity);
+            }
+            else
+            {
+                context.SetError("invalid_grant", "The user name or password is incorrect.");
+            }
+        }
+    }
+}
---
+++ api/Startup.cs
@@ -0,0 +1,39 @@
+﻿using api;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(WebApi.Startup))]
+namespace WebApi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            ConfigureOAuth(app);
+
+            WebApiConfig.Register(config);
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromSeconds(60),// TimeSpan.FromDays(1),
+                Provider = new SimpleAuthorizationServerProvider()
+            };
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+        }
+    }
+}
